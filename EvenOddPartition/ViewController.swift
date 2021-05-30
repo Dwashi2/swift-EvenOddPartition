a296bab99677de1623fc11484bf284cd2656f060
Daniel Washington <danielvertigo@hotmail.com>
2021-05-30 13:14:51 -0300

    Files

--- EvenOddPartition/ViewController.swift
+++ EvenOddPartition/ViewController.swift
@@ -5,6 +5,27 @@
 //  Created by Daniel Washington Ignacio on 30/05/21.
 //
 
+
+/*
+ Write a function that partitions the array into two subarrays: one with all even integers, and the other with all odd integers. Return your result in the following format:
+
+ [[evens], [odds]]
+ Examples
+
+ evenOddPartition([5, 8, 9, 2, 0]) ➞ [[8, 2, 0], [5, 9]]
+
+ evenOddPartition([1, 0, 1, 0, 1, 0]) ➞ [[0, 0, 0], [1, 1, 1]]
+
+ evenOddPartition([1, 3, 5, 7, 9]) ➞ [[], [1, 3, 5, 7, 9]]
+
+ evenOddPartition([]) ➞ [[], []]
+ Notes
+
+ Return two empty subarrays if the input is an empty array.
+ Keep the same relative ordering as the original array.
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +33,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print(self.evenOddPartition([5, 8, 9, 2, 0]))
+        print(self.evenOddPartition([1, 0, 1, 0, 1, 0]))
+        print(self.evenOddPartition([1, 3, 5, 7, 9]))
+        print(self.evenOddPartition([]))
     }
 
+    
+    func evenOddPartition(_ arr: [Int]) -> [[Int]] {
+        var arrayResultEven: [Int] = []
+        var arrayResultOdd: [Int] = []
+        var arrayResult: [[Int]] = [[]]
+        arrayResult.removeAll()
+        for n in arr{
+            if n % 2 == 0 {
+                arrayResultEven.append(n)
+            }else{
+                arrayResultOdd.append(n)
+            }
+        }
+        arrayResult.append(arrayResultEven)
+        arrayResult.append(arrayResultOdd)
+        return arrayResult
+        
+    }
 
 }
 
